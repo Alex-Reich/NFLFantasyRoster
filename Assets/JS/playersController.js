772346ca34de8c1143c4652e5a42213cd8b001c8
Alex Reich <atrain.reich@gmail.com>
2018-05-04 16:57:05 -0600

    Added search functionality, began add&remove features

--- Assets/JS/playersController.js
+++ Assets/JS/playersController.js
@@ -1,5 +1,20 @@
 function PlayersController() {
-    var playersService = new PlayersService(drawPlayers)
+
+    var playersService = new PlayersService(ready)
+
+    function ready(){
+        document.getElementById('spinner').innerHTML = ''
+    }
+
+   this.search = function search(e){
+        e.preventDefault()
+        debugger
+        var query = e.target.query.value
+        var results = playersService.search(query)
+        drawPlayers(results)
+        }
+        
+
 
     function drawPlayers(players) {
         var template = "<h1>Pool of Playas</h1>"
@@ -9,9 +24,10 @@ function PlayersController() {
             <div class="card align-items-center">
                 <img src="${player.photo}" alt="">
                 <div class="card-body align-items-center">
-                    <h1>${player.firstname} ${player.lastname}</h1>
+                    <h1>${player.fullname}</h1>
                     <h2>Position: ${player.position}</h2>
                     <h2>Team: ${player.pro_team}</h2>
+                    <button onclick="app.controllers.playerController.addToMyTeam${player.id}">Add to my team</button>
                 </div>
             </div>
             `
@@ -19,9 +35,32 @@ function PlayersController() {
         document.getElementById("playersList").innerHTML = template
     }
 
+        function drawMyTeam(players) {
+            var template = "<h1>The Squad</h1>"
+            for (var i = 0; i < players.length; i++) {
+                var player = players[i];
+                template += `
+                <div class="card align-items-center">
+                    <img src="${player.photo}" alt="">
+                    <div class="card-body align-items-center">
+                        <h1>${player.fullname}</h1>
+                        <h2>Position: ${player.position}</h2>
+                        <h2>Team: ${player.pro_team}</h2>
+                    </div>
+                </div>
+                `
+            }
+            document.getElementById("myTeam").innerHTML = template
+        }
 
 
+      this.addToMyTeam =  function addToMyTeam(id) {
+        playersService.addToMyTeam(id, drawMyTeam);
+        }
 
+        this.removeFromteam = function removeFromteam(id) {
+            playersService.removeFromTeam(id, drawMyTeam)
+        }
 
 
 
